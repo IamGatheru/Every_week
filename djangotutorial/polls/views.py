6fb1428745027beeb29eac10b7bf9800e3feea17
IamGatheru <davidgatheru490@gmail.com>
2025-02-23 23:47:49 +0300

    Writing Templates in a few

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -2,4 +2,19 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return Httpresponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're  voting on question %s." % question_id)
