5e5908a3b115adfe292ec1cd4938f316c1a9a2c2
IamGatheru <davidgatheru490@gmail.com>
2025-02-24 01:06:52 +0300

    Introduced namespacing, emphasis on non-coupling

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
@@ -18,3 +26,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're  voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
